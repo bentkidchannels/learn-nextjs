8be229949a98ca7e7b73266c6ea1f9c689242d51
HaVo <ha.vo@deliveree.com>
2023-03-05 22:06:07 +0700

    Routing - Add routing page demo

---
+++ pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface PostListProps {
+}
+
+export default function PostList (props: PostListProps) {
+  return (
+    <div>
+      Post List
+    </div>
+  );
+}
